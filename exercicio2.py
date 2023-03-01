7d01160f5f344822256e475611ebab8604fdb6f5
Guilherme Bezerra <guilhermebfneto@gmail.com>
2023-03-01 17:39:35 -0300

    Commit inicial; exercício 2 realizado

---
+++ exercicio2.py
@@ -0,0 +1,25 @@
+def pertenceFibonacci(numero):
+    # Variáveis com os valores iniciais da sequência
+    a = 0
+    b = 1
+
+    while a <= numero:
+        if numero == a:
+            return True
+
+        a, b = b, a + b
+
+    return False
+
+
+def main():
+    numero = int(input("Digite um número inteiro: "))
+
+    if pertenceFibonacci(numero):
+        print(f"O número {numero} pertence à sequência de Fibonacci")
+    else:
+        print(f"O número {numero} não pertence à sequência de Fibonacci")
+
+
+if __name__ == "__main__":
+    main()
